f85c11171a8db10da8f06e313c2ecd5dee69bb7d
Bermx97 <bermxtf@gmail.com>
2025-05-05 16:52:38 +0200

    add envelopes.js

---
+++ envelopes.js
@@ -0,0 +1,26 @@
+const envelopes = [{
+    id: 1,
+    category: 'groceries',
+    limit: 6000
+}, {
+    id: 2,
+    category: 'gas',
+    limit: 5000
+}, {
+    id: 3,
+    category: 'clothing',
+    limit: 1000
+}, {
+    id: 4,
+    category: 'health and grooming',
+    limit: 2000
+}, {
+    id: 5,
+    category: 'Natalia',
+    limit: 10000
+}, {id: 6,
+    category: 'me'
+}
+]
+
+module.exports = { envelopes }
